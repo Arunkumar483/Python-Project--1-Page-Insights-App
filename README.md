32be2ccbabd567715d9cb7a33b61384d1f332d17
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-11-02 10:19:28 +0530

    readme  and description change

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Python Project -1 Page Insights App
- 
+
+This webapp measures the performance of the webpage.
+
+WORKFLOW
+
+Get the url to be assessed from the user input.
+
+Return the Performance metrics by parsing the response from the API.
+
+
+API 
+The PageSpeed Insights API returns real-world data from the Chrome User Experience Report and lab data from Lighthouse.
+
+TOOLS USED
+Flask
+wkhtmltopdf(not finalized)
+
--- pyproject/app.py
+++ pyproject/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, json,render_template
 import requests
-#Standard :WSGI -> STANDARD THAT IS USED TO COMMUNICATE MY APP TO SERVER
+#Standard :WSGI -> Standard that is used for communication between appliication and server.
 app=Flask(__name__)
 #Decorator binded with function
 @app.route('/',methods=['Get'])
@@ -8,9 +8,14 @@ def index():
     
     api_key = 'AIzaSyAhSbk4l49jwrWO9iBaD_hJeLg3elYjSEw'
     search_api_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://www.google.com&key='+api_key
+    
     #getting the information response from the specified url
     response = requests.get(search_api_url)
+    
+    #convert json string into python dictionary
     data=json.loads(response.content)
+    
+    #getting necessary parameters from the response's dictionary (data) 
     captcha=data["captchaResult"]
     timestamp=data["analysisUTCTimestamp"]
     performance=data["loadingExperience"]
@@ -21,7 +26,6 @@ def index():
 
 @app.route('/results')
 def result():
-    #return json.jsonify({"hello":"gk"})
     return "<h1 align=center>Page Speed Analyser</h1>"
 
 
