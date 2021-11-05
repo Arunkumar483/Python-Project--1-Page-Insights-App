3b865e4cfef792d57e3e310331c9d938597c7aeb
Arunkumar <52123143+Arunkumar483@users.noreply.github.com>
2021-11-05 12:28:41 +0530

    get method change

--- pyproject/app.py
+++ pyproject/app.py
@@ -10,14 +10,10 @@ app=Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/results',methods=['GET','POST'])
+@app.route('/results',methods=['GET'])
 def result():
-    if request.method == 'POST':
-        urltobechecked=request.form['url']
-    
-
-    
-
+    print(request.args.get('url'))
+    urltobechecked=request.args.get('url')
     api_key = 'AIzaSyAhSbk4l49jwrWO9iBaD_hJeLg3elYjSEw'
     search_api_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url='+urltobechecked+'/&key='+api_key
     
@@ -41,4 +37,4 @@ def result():
     return render_template('results.html',urltobechecked=urltobechecked,data=data,captcha=captcha,timestamp=timestamp,lighthouseResult=lighthouseResult,analyzedurl=analyzedurl,auditsdata=auditsdata)
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
